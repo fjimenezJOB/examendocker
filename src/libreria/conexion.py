55550add5a26a703f42781bb124157719cc17604
fjimenezJOB <fjimenezjob@gmail.com>
2020-02-10 11:29:35 +0100

    Montaje de la pagina

--- src/libreria/conexion.py
+++ src/libreria/conexion.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from flask import session
-import datetime
+import time
 
 
 MONGO_URL_ATLAS = 'mongodb+srv://franjimenez:Francisco1231998@develop-0hasi.mongodb.net/test?retryWrites=true&w=majority'
@@ -15,6 +15,19 @@ def insertarParticipante(nombre, email):
     """
         Inserta los participantes en la base de datos. ( Nombre de participante, correo electronico y la fecha actual. )
     """
-    fecha = datetime.date.today()
+    resultados = collection.find({'email': email}, {'_id': 0, 'email': 1, 'activo': 1})
+
+    lista = []
+    for documento in resultados:
+        lista.append(documento)
+    
+    for documento in lista:
+        if email == documento['email']:
+            error = True
+            return error
+
+    fecha = time.strftime("%d/%m/%y")
     print (fecha)
     collection.insert_one({'nombre': nombre, 'email': email, 'fecha': fecha ,'activo' : 1})
+
+insertarParticipante('Fran Jimenez', 'franloloco@gmail.com')
--- src/main.py
+++ src/main.py
@@ -1,17 +1,38 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import os
-
+from libreria.conexion import *
 
 app = Flask(__name__)
 
+app.secret_key = 'esto-es-una-clave-muy-secreta'
 
-@app.route("/", methods=['POST','GET'])
-def home():
+@app.route("/", methods=['POST', 'GET'])
+def index():
     if request.method == 'POST':
-        return render_template('index.html')
-    return render_template('index.html')
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        print(nombre)
+        session['nombre'] = nombre
+        session['email'] = email
+        error = insertarParticipante(nombre, email)
+        if error:
+            return render_template('index.html', error = error)
+        return redirect('home')
+    context = {
+        'titulo': 'Bienvenido !!',
+        'subtitulo': 'Solo tienes que registrar tu Nombre y tu correo electronico para participar.'
+    }
+    return render_template('index.html', **context)
 
+@app.route('/home')
+def home():
+    context = {
+        'titulo' : 'Bienvenido al sorteo ' + session['nombre'],
+        'subtitulo' : 'Estas registrado como ' + session['email']
+    }
+    return render_template('home.html', **context )
+    
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run('0.0.0.0', port=port, debug=True)
